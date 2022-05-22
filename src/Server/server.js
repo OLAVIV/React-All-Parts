26efe5dd155abb287d3f8a3716fdc560ac416dc7
olaVI <olavi.kurki2004@gmail.com>
2022-05-23 02:31:50 +0300

    Final version

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,82 +1,139 @@
-var express = require('express');
-var cors = require('cors')
-var app = express();
-var fs = require("fs");
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '.mongozilla.', 'public');
+const port = process.env.PORT || 3001;
+app.use(express.static(publicPath));
 
-let data = {
-  "reminders": [
-    {
-      "name": "Buy some eggs",
-      "timestamp": "2021-11-10T13:00:00.141Z",
-      "id": 1
-    },
-    {
-      "name": "Make an omelette",
-      "timestamp": "2021-11-11T08:00:00.141Z",
-      "id": 2
-    },
-    {
-      "name": "Wash dishes",
-      "timestamp": "2021-11-11T09:00:00.000Z",
-      "id": 3
-    },
-    {
-      "name": "Buy more eggs",
-      "timestamp": "2021-11-11T13:00:00.000Z",
-      "id": 4
-    }
-  ]
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
+
+import React from 'react';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+class ReminderForm extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <form>
+      <div>
+        Name: <input onChange={this.props.onChangeName} />
+      </div>
+      <div>
+        At time: <input onChange={this.props.onChangeDate} />
+      </div>
+      <div>
+        <button onClick={this.props.onAddReminder}>Add</button>
+      </div>
+    </form>
+  }
 }
 
-app.use(express.json())
-app.use(cors())
-
-app.get("/api/reminders/:id", function (req, res) {
-  const reminder = data.reminders.find(r => r.id == req.params.id)
-  if (!reminder) {
-    res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-    res.end();
-    return
+class ReminderItem extends React.Component {
+  constructor(props) {
+    super(props)
   }
-  res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-  res.write(JSON.stringify(reminder));
-  res.end();
-})
-
-app.delete("/api/reminders/:id", function (req, res) {
-  const reminder = data.reminders.find(r => r.id == req.params.id)
-  if (!reminder) {
-    res.status(404).send("Not Found");
-    return
+  render() {
+    return <div>
+      {new Date(this.props.timestamp).toDateString() + " "}
+      {this.props.name}
+      <button onClick={(e) => this.props.onRemoveItem(this.props.itemId)}>
+        Remove
+      </button>
+    </div>
   }
-  data = { reminders: data.reminders.filter(r => r.id != req.params.id) }
-  res.status(200).header({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }).send({});
-})
-
-app.get("/api/reminders/", function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-  res.write(JSON.stringify(data));
-  res.end();
-})
-
-app.post("/api/reminders", function (req, res) {
-  console.log(req.body.name + " " + req.body.timestamp)
-  if (!req.body.name || !req.body.timestamp) {
-    res.status(406).send("Invalid name or timestamp");
-    return
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reminders: [],
+      newName: '',
+      newDate: ''
+    }
   }
-  if (data.reminders.find(r => r.name == req.body.name)){
-    res.status(400).send("Same reminder already exists!");
-    return
-  }
-  let newId = Math.trunc(Math.random() * 1000000)
-  let newReminder = { id: newId, name: req.body.name, timestamp: req.body.timestamp }
-  data.reminders.push(newReminder)
-  res.status(200).header({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }).send(JSON.stringify(newReminder));
-})
 
-var server = app.listen(3001, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-})
+  componentDidMount() {
+    let app = this
+    axios.get("https://reminderserver.herokuapp.com/api/reminders")
+      .then(function (response) {
+        app.setState({
+          reminders: response.data.reminders
+        })
+        console.log(response);
+      })
+  }
+
+  onChangeName = (e) => {
+    this.setState({ newName: e.target.value });
+  }
+
+  onChangeDate = (e) => {
+    this.setState({ newDate: e.target.value });
+  }
+
+  addReminder = (e) => {
+    e.preventDefault();
+    let reminders = this.state.reminders
+    if (reminders.some(r => this.state.newName == r.name)) {
+      alert("Reminder already exists! Cannot add the same name twice.")
+      return;
+    }
+    let date = new Date(this.state.newDate)
+    if (date == "Invalid Date") {
+      alert("Invalid Date! Cannot add in this format use this instead,                         yyyy-mm-ddthh:mm")
+      return;
+    }
+    const newReminder = {
+      name: this.state.newName,
+      timestamp: date.toISOString()
+    }
+    let app = this
+    axios.post("https://reminderserver.herokuapp.com/api/reminders", newReminder)
+      .then(function (response) {
+        reminders.push(response.data)
+        app.setState({ reminders: reminders });
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
+
+  removeItem = (itemId) => {
+    if (!window.confirm("Do you really want to remove this item?"))
+      return
+    axios.delete("https://reminderserver.herokuapp.com/api/reminders/" + itemId)
+      .then(() => {
+        let reminders = this.state.reminders.filter(r => r._id != itemId)
+        this.setState({ reminders: reminders });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Reminders</h2>
+        <ReminderForm onChangeName={this.onChangeName} onChangeDate={this.onChangeDate} onAddReminder={this.addReminder} />
+        <h2>Reminders</h2>
+        {this.state.reminders.map((reminder) => (
+          <div key={reminder._id}>
+            <ReminderItem name={reminder.name} timestamp={reminder.timestamp} itemId={reminder._id} onRemoveItem={this.removeItem} />
+          </div>
+        ))}
+        {/* debug: {this.state.newName} */}
+        <Link to='./'>Back</Link>
+      </div>
+    )
+  }
+}
+
+export default App
